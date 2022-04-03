1f38d82020349c626a9f577ba5da8ec8fe69bf5f
Mosleh Mahamud <mosleh.edu@gmail.com>
2022-04-03 18:13:48 +0200

    work in progress

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import EDA as eda
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +12,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    augger = eda.Enkel_Data_Augmentation()
+    augger.synonym_replacement("Branden vid oljedepån övre på höjd",1)
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
