cbf7e142a36096cb0ae6914f45c8960f34876986
Mosleh Mahamud <mosleh.edu@gmail.com>
2022-04-07 20:10:39 +0200

    cropping started

---
+++ Text_Cropping.py
@@ -0,0 +1,11 @@
+
+
+class cropper():
+    def __init__(self):
+        self.percentage = 0.25
+        pass
+    #https://colab.research.google.com/drive/1oQkPlCREnhiAz0zTifgVi0YWcE6hIzi6#scrollTo=EFMPJVBTNJNh
+
+    def crop(self, sentence):
+        sentence = sentence.split()
+
