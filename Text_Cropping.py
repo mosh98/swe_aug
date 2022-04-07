afedf4a68e488673fb2096055081e4162c348d12
Mosleh Mahamud <mosleh.edu@gmail.com>
2022-04-07 20:19:00 +0200

    cropping fixed

--- Text_Cropping.py
+++ Text_Cropping.py
@@ -1,4 +1,4 @@
-
+import math
 
 class cropper():
     def __init__(self):
@@ -6,6 +6,18 @@ class cropper():
         pass
     #https://colab.research.google.com/drive/1oQkPlCREnhiAz0zTifgVi0YWcE6hIzi6#scrollTo=EFMPJVBTNJNh
 
-    def crop(self, sentence):
-        sentence = sentence.split()
+    def text_fragmeter(self, sentence):
+
+        framented = []
+        words = sentence.split(" ")
+        sen_length = len(words)
+        chunk_length = sen_length * self.percentage
+
+        for i in range(0, sen_length, chunk_length):
+            #print(words[i:i + n])
+            framented.append(words[i:i + chunk_length])
+
+        return framented
+
+
 
