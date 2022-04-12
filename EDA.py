5104a1f535339c850f244eda4f33789526318c56
Mosleh Mahamud <mosleh.edu@gmail.com>
2022-04-12 11:09:21 +0200

    random swap implemented

--- EDA.py
+++ EDA.py
@@ -258,7 +258,28 @@ class Enkel_Data_Augmentation():
         random_idx = random.randint(0, len(new_words)-1)
         new_words.insert(random_idx, random_synonym)
 
+    ########################################################################
+    # Random swap
+    # Randomly swap two words in the sentence n times
+    ########################################################################
 
+    def random_swap(self,words, n):
+        new_words = words.copy()
+        for _ in range(n):
+            new_words = self.swap_word(new_words)
+        return new_words
+
+    def swap_word(self,new_words):
+        random_idx_1 = random.randint(0, len(new_words) - 1)
+        random_idx_2 = random_idx_1
+        counter = 0
+        while random_idx_2 == random_idx_1:
+            random_idx_2 = random.randint(0, len(new_words) - 1)
+            counter += 1
+            if counter > 3:
+                return new_words
+        new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1]
+        return new_words
 
     def enkel_augmentation(self, sentence, alpha_sr=0.1, alpha_ri=0.1, alpha_rs=0.1, alpha_rd=0.1, num_aug=9):
 
@@ -303,6 +324,13 @@ y
             for _ in range(num_new_per_technique):
                 a_words = self.random_deletion(words, alpha_rd)
                 print(a_words)
+                augmented_sentences.append(' '.join(a_words))
+
+        #Random Swap
+        if (alpha_rs > 0):
+            n_rs = max(1, int(alpha_rs * num_words))
+            for _ in range(num_new_per_technique):
+                a_words = self.random_swap(words, n_rs)
                 augmented_sentences.append(' '.join(a_words))
 
 
